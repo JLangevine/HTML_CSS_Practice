6ec45ab8f24e9ac1317e4b9831598af18803c35b
JewelLangevineArbor <jewel@arbor.io>
2018-07-27 01:01:50 -0400

    made sure the app was working, all is well

--- js/shuffle2.js
+++ js/shuffle2.js
@@ -5,15 +5,15 @@ const { JSDOM } = jsdom;
 
 function shuffle2(){
 		var word = "Changed";
-		global.dom = new JSDOM('<!doctype html><html><body></body></html>');
+		/*global.dom = new JSDOM('<!doctype html><html><body></body></html>');
 global.window = dom.window;
 global.document = dom.window.document;
 global.createElement = dom.window.document.createElement;
-global.getElementById = dom.window.document.getElementById;
+global.getElementById = dom.window.document.getElementById;*/
 		
-		//document.getElementById("word").innerHTML = word;
+		document.getElementById("word").innerHTML = word;
 		//document.body.firstChild.innerHTML = word;
-		dom.window.document.body.firstChild.innerHTML = "Changed";
+		//dom.window.document.body.firstChild.innerHTML = "Changed";
 	};
 
 module.exports = shuffle2();
